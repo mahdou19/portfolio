3d5064f4a835ea8a85bdc1881451d7e3dc57b807
Mamadou <mamadou.sall@42c.fr>
2022-12-27 22:02:03 +0100

    implement welcome page

--- src/components/mainpage/welcome/Welcome.jsx
+++ src/components/mainpage/welcome/Welcome.jsx
@@ -1,12 +1,25 @@
 import React from "react";
 import { useStyles } from "../../../style/welcome/index.jsx";
-import { Box } from "@mui/material";
+import { Box, Typography, useTheme } from "@mui/material";
 
 function Welcome() {
+
+  const theme = useTheme()
   const classes = useStyles();
   return (
-    <Box className={classes.welcomeHeader}>
-      <Box className={classes.welcomeContainer}></Box>
+    <Box className={classes.welcomeContainer}>
+      <Box className={classes.welcomeHeader}>
+        <Typography variant="h3" className={classes.welcomeTitle} sx={{mb:5, fontFamily: 'Bungee Inline', [theme.breakpoints.down('sm')]: {
+            fontFamily: 'Bangers',
+            fontWeight: 400,
+            
+          },}}>
+          Developpeur Web FullStack
+        </Typography>
+        <Typography variant="h6" className={classes.welcomeDescription}>
+          Je m'appelle Mamadou SALL , j’ai 26 ans et je suis Développeur Web Full Stack. Je suis alternant en Master I à l'école ESTIAM de Paris en WMD (Web & Mobile Development) et chez 42 Consulting depuis Octobre 2021, j'ai une solide formation en informatique et une expérience professionnelle en développement Web.J'ai travaillé sur de nombreux projets, en utilisant différents langages de programmation et technologies.
+        </Typography>
+      </Box>
     </Box>
   );
 }
--- src/style/welcome/index.jsx
+++ src/style/welcome/index.jsx
@@ -1,12 +1,40 @@
 import { Colors } from "../theme/index.jsx";
+import { makeStyles } from "@material-ui/core";
 
-const { makeStyles } = require("@material-ui/core");
+import background from '../../assets/images/image2.jpg'
 
 export const useStyles = makeStyles((theme) => ({
-    welcomeHeader: {
+    welcomeContainer: {
         width: "100%",
-        height: "90vh",
-        backgroundColor: Colors.green,
-        margin: 0
+        minHeight: "90vh",
+        height: "auto",
+        background: `linear-gradient(to bottom right,#04303140, #00606473),url(${background})`,
+        backgroundSize: "cover",
+        backgroundRepeat: "no-repeat"
+    },
+    welcomeHeader : {
+        padding: "50px",
+        width: "100%",
+        minHeight: "90vh",
+        height: "auto",
+        display: "flex",
+        flexFlow: "column wrap",
+        justifyContent: "center",
+        color: Colors.white ,
+        [theme.breakpoints.down('xs')]: {
+            padding: "35px",
+            fontFamily: 'Bungee Inline'
+          },
+        
+    },
+    welcomeTitle : {
+        fontSize: "5em",
+        textAlign: "center",
+       
+    },
+    welcomeDescription: {
+        fontSize: "5em",
+        textAlign: "center",
+       
     }
 }))
