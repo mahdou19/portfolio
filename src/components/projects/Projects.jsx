3992674f86d4930a39c237e60b839eb50efade4e
Mamadou <mamadou.sall@42c.fr>
2022-12-30 04:52:44 +0100

    fix style footer

--- src/components/projects/Projects.jsx
+++ src/components/projects/Projects.jsx
@@ -1,49 +1,58 @@
-import React from 'react'
-import CardProject from './CardProject.jsx'
-import { Container } from '@mui/system'
-import { Box, Typography, useTheme } from '@mui/material'
-import { useStyles } from '../../style/skills/index.jsx'
+import React from "react";
+import CardProject from "./CardProject.jsx";
+import { Container } from "@mui/system";
+import { Box, Typography, useTheme } from "@mui/material";
+import { useStyles } from "../../style/skills/index.jsx";
+import Footer from "../common/footer/Footer.jsx";
 
 function Projects() {
-  const classes = useStyles()
-  const theme = useTheme()
+  const classes = useStyles();
+  const theme = useTheme();
   return (
-    <Container sx={{p:0}}>
-          <Box
-        className={classes.skillHeader}
-        sx={{
-          mt: 2,
-          [theme.breakpoints.down("sm")]: {
-            fontFamily: "Bangers",
-            fontSize: "1em",
-            justifyContent: "center",
-            alignItems: "center",
-          },
-        }}
-      >
-        <Typography
-          variant="h2"
-          gutterBottom
-          className={classes.headerTitle}
+    <>
+      <Container sx={{ p: 0 }}>
+        <Box
+          className={classes.skillHeader}
           sx={{
-            my: 3,
-            fontWeight: 400,
-            fontSize: "2.5em",
-            fontFamily: "Bungee Inline",
+            mt: 2,
             [theme.breakpoints.down("sm")]: {
               fontFamily: "Bangers",
-              fontWeight: 400,
+              fontSize: "1em",
+              justifyContent: "center",
+              alignItems: "center",
             },
           }}
         >
-          PROJETS
-        </Typography>
-      </Box>
-      <Box >
-      <CardProject/>
-      </Box>
-    </Container>
-  )
+          <Typography
+            variant="h2"
+            gutterBottom
+            className={classes.headerTitle}
+            sx={{
+              my: 3,
+              fontWeight: 400,
+              fontSize: "2.5em",
+              fontFamily: "Bungee Inline",
+              [theme.breakpoints.down("sm")]: {
+                fontFamily: "Bangers",
+                fontWeight: 400,
+              },
+            }}
+          >
+            PROJETS
+          </Typography>
+        </Box>
+        <Box sx={{ mb: 5,
+        minHeight: "57vh",
+        [theme.breakpoints.down("sm")]: {
+          px: 0,
+        },
+      }}>
+          <CardProject />
+        </Box>
+      </Container>
+      <Footer />
+    </>
+  );
 }
 
-export default Projects
+export default Projects;
