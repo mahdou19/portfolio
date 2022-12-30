2cdc794785ec785158e62c3388b5fb1ad5810792
Mamadou <mamadou.sall@42c.fr>
2022-12-30 04:31:23 +0100

    fetch data project and display in the card

--- src/components/projects/CardProject.jsx
+++ src/components/projects/CardProject.jsx
@@ -6,47 +6,52 @@ import CardMedia from '@mui/material/CardMedia';
 import Typography from '@mui/material/Typography';
 import { Button, CardActionArea, CardActions, Grid } from '@mui/material';
 import GitHubIcon from "@mui/icons-material/GitHub";
+import { makeStyles } from '@material-ui/core';
+import { Colors } from '../../style/theme/index.jsx';
+import { projects } from '../../data/data.js';
 
+const useStyles = makeStyles((theme) => ({
+  welcomeContainer: {
+    '@media (min-width: 0px)': {
+      padding: 0,
+      margin:0,
+      background: Colors.green
+    },
+
+  },
+}));
 function CardProject() {
   const theme = useTheme();
+  const classes = useStyles()
   return (
-    <Grid sx={{ flexGrow: 1, m:0 }} container spacing={2} >
-     <Grid container spacing={{ xs: 2, md: 3 }} columns={{ xs: 3, sm: 8, md: 12 }}  alignItems="center" direction="row"
-  justifyContent="center" sx={{  [theme.breakpoints.down("md")]: {
-    mx:5, p:0
-  }, [theme.breakpoints.down("sm")]: {
-    mx:0, p:0
-  }}}>
-      {[0, 1, 2].map((value) => (
-         <Grid item xs={2} sm={4} md={4} sx={{  [theme.breakpoints.down("md")]: {
-          m:0, p:0
-        }, [theme.breakpoints.down("sm")]: {
-          m:0, p:0
-        }}}>
-        <Card sx={{ maxWidth: 345, textAlign: "center", [theme.breakpoints.down("md")]: {
-    width: "100%"
+    <Grid sx={{ flexGrow: 1, m:0, p:0 }} container spacing={2} alignItems="center">
+     <Grid container  columns={{ xs: 3, sm: 8, md: 12 }}  alignItems="center" direction="row"
+  justifyContent="center" >
+      {projects.map((project, index) => (
+         <Grid  xs={2} sm={4} md={4} >
+        <Card key={index} sx={{ maxWidth: 345, textAlign: "center", mb: 2, [theme.breakpoints.down("md")]: {
+    width: "100%", p: 0, 
   }}} >
       <CardActionArea>
         <CardMedia
           component="img"
           height="140"
-          image="https://images.unsplash.com/photo-1618477388954-7852f32655ec?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=764&q=80"
+          image={project.image}
           alt="green iguana"
         />
         <CardContent>
           <Typography gutterBottom variant="h5" component="div">
-            Lizard
+            {project.title}
           </Typography>
           <Typography variant="body2" color="text.secondary">
-            Lizards are a widespread group of squamate reptiles, with over 6,000
-            species, ranging across all continents except Antarctica
+          {project.desc}
           </Typography>
         </CardContent>
       </CardActionArea>
       <CardActions sx={{justifyContent: "center"}}>
       <GitHubIcon
           onClick={() => {
-            window.location.href = "https://github.com/";
+            window.location.href = project.link
           }}
           sx={{ fontSize: 40 }}
        
--- src/components/projects/Projects.jsx
+++ src/components/projects/Projects.jsx
@@ -1,12 +1,45 @@
 import React from 'react'
 import CardProject from './CardProject.jsx'
 import { Container } from '@mui/system'
-import { Box } from '@mui/material'
+import { Box, Typography, useTheme } from '@mui/material'
+import { useStyles } from '../../style/skills/index.jsx'
 
 function Projects() {
+  const classes = useStyles()
+  const theme = useTheme()
   return (
     <Container sx={{p:0}}>
-      <Box>
+          <Box
+        className={classes.skillHeader}
+        sx={{
+          mt: 2,
+          [theme.breakpoints.down("sm")]: {
+            fontFamily: "Bangers",
+            fontSize: "1em",
+            justifyContent: "center",
+            alignItems: "center",
+          },
+        }}
+      >
+        <Typography
+          variant="h2"
+          gutterBottom
+          className={classes.headerTitle}
+          sx={{
+            my: 3,
+            fontWeight: 400,
+            fontSize: "2.5em",
+            fontFamily: "Bungee Inline",
+            [theme.breakpoints.down("sm")]: {
+              fontFamily: "Bangers",
+              fontWeight: 400,
+            },
+          }}
+        >
+          EXPERIENCES
+        </Typography>
+      </Box>
+      <Box >
       <CardProject/>
       </Box>
     </Container>
